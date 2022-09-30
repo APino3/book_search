0b7e2a5c9e845a5593f1d6b7bf14fea774eea185
Albalicia Espino <albaliciae@gmail.com>
2022-09-30 12:38:49 -0500

    resolver.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,6 @@
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
 const resolvers = {
   Query: {
     getSingleUser: async (parent, { params }) => {
@@ -13,20 +16,18 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async (parent, args) => {
+    createUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
       return { token, user };
     },
     login: async (parent, { email, password }) => {
-      const user = await User.findOne({
-        $or: [{ username: body.username }, { email: body.email }],
-      });
+      const user = await User.findOne({ username: email });
       if (!user) {
         return res.status(400).json({ message: "Can't find this user" });
       }
 
-      const correctPw = await user.isCorrectPassword(body.password);
+      const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
         throw new AuthenticationError("Incorrect credentials");
@@ -35,50 +36,30 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    addThought: async (parent, args, context) => {
+    saveBook: async (parent, args, context) => {
+      console.log(" args ", args, " context ", context);
+
       if (context.user) {
-        const thought = await Thought.create({
-          ...args,
-          username: context.user.username,
-        });
-
-        await User.findByIdAndUpdate(
-          { _id: context.user._id },
-          { $push: { thoughts: thought._id } },
-          { new: true }
-        );
-
-        return thought;
+        try {
+          const updateUser = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $addToSet: { savedBooks: args } },
+            { new: true, runValidators: true }
+          );
+          return res.json(updatedUser);
+        } catch (err) {
+          console.log(err);
+          return res.status(400).json(err);
+        }
       }
 
       throw new AuthenticationError("You need to be logged in!");
     },
-    addReaction: async (parent, { thoughtId, reactionBody }, context) => {
+    deleteBook: async (parent, args, context, info) => {
       if (context.user) {
-        const updatedThought = await Thought.findOneAndUpdate(
-          { _id: thoughtId },
-          {
-            $push: {
-              reactions: { reactionBody, username: context.user.username },
-            },
-          },
-          { new: true, runValidators: true }
-        );
-
-        return updatedThought;
-      }
-
-      throw new AuthenticationError("You need to be logged in!");
-    },
-    addFriend: async (parent, { friendId }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $addToSet: { friends: friendId } },
-          { new: true }
-        ).populate("friends");
-
-        return updatedUser;
+        await Book.findOneAndRemove({ bookId: args.bookId });
+        const currBooks = await User.find({ username: context.user.username });
+        return currBooks;
       }
 
       throw new AuthenticationError("You need to be logged in!");
