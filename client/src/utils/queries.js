f67ff32199d3ef8ac57b09fddfdd027783e283f6
Albalicia Espino <albaliciae@gmail.com>
2022-09-29 21:58:29 -0500

    added queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,31 +1,57 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_THOUGHTS = gql`
-  query thoughts($username: String) {
-    thoughts(username: $username) {
-      _id
-      thoughtText
-      createdAt
-      username
-      reactionCount
-      reactions {
-        _id
-        createdAt
-        username
-        reactionBody
-      }
-    }
-  }
-`;
-
 export const QUERY_BOOKS = gql`
-  query savedBooksQL($username: String) {
-    savedBooksQL(username: $username) {
+  query GetBooks($username: String) {
+    getBooks(username: $username) {
       _id
       username
       email
       password
       savedBooks {
+        _id
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+export const MUTATION_CREATE_USER = gql`
+  mutation CreateUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const MUTATION_SAVE_BOOK = gql`
+  mutation SaveBook(
+    $authors: [String]!
+    $description: String!
+    $image: String!
+    $link: String!
+    $title: String!
+  ) {
+    saveBook(
+      authors: $authors
+      description: $description
+      image: $image
+      link: $link
+      title: $title
+    ) {
+      _id
+      username
+      email
+      password
+      saveBooks {
         _id
         authors
         description
